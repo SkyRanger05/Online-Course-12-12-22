ba7058ef775230b73b41e875699a0550bb455210
SkyRanger05 <120250815+SkyRanger05@users.noreply.github.com>
2022-12-26 20:53:49 -0800

    Variables exercise

---
+++ UC_BERKLEY_BOOTCAMP_FinTech_coding-exercises/variables.py
@@ -0,0 +1,31 @@
+# Create variables
+floor_area_sq_ft = 1500
+sale_item = "dining table"
+auto_maintenance = "oil change"
+curriculum = "English, Math, Science"
+# Print the variables
+print(floor_area_sq_ft, sale_item, auto_maintenance, curriculum)
+# Prints the data type of each declared variable
+print(type(floor_area_sq_ft))
+print(type(sale_item)) 
+print (type(auto_maintenance))
+print(type(curriculum))
+# Using variable names in calculations
+date_of_birth_year = 1976
+current_year = 2022
+age = current_year - date_of_birth_year
+print(age)
+# Updating variables using assignment
+
+
+# Substituting/formatting variable
+print(f"The house has about {floor_area_sq_ft} sq. ft' of living space.")
+print(f"This {sale_item} is for sale at $70.")
+print(f"On Saturday, I will do {auto_maintenance} my Ferrari.")
+print(f"This fall, I will be signing up classes for {curriculum}")
+print(f"I'm {age} years of age.")
+# Two number values will be added
+
+# Two string values will be concatenated
+
+# Variable naming conventions
