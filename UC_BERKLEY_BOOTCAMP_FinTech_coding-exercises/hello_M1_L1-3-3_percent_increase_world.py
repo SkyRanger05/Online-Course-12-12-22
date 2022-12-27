cbe8f03c5edc9f979efc6a8e3aa03f5013ff3f9f
SkyRanger05 <120250815+SkyRanger05@users.noreply.github.com>
2022-12-26 20:40:37 -0800

    First bootcamp course exercise
    UC Berkley FinTech Bootcamp course exercises/activities....(starting out as a beginner's level with no prior programming know-how...)

---
+++ UC_BERKLEY_BOOTCAMP_FinTech_coding-exercises/hello_M1_L1-3-3_percent_increase_world.py
@@ -0,0 +1,23 @@
+"""Hello Variable World."""
+
+# 1. @TODO: Create a variable called `original_price`
+# and assign it the value of `198.87`.
+original_price = 198.87
+
+# 2. @TODO: Create a variable called `current_price`
+# and assign it the value `254.32`.
+current_price = 254.32
+
+# 3. @TODO: Create a variable called `increase` and assign the
+# difference between the current price and the original price.
+increase = current_price - original_price
+
+# 4. @TODO: Divide `increase` by `original_price` and multiply that by 100.
+# Then assign the results to the variable called `percent_increase`.
+percent_increase = ( increase / original_price ) * 100
+
+# 5. @TODO: Print all of the variables.
+print(original_price)
+print(current_price)
+print(increase)
+print(percent_increase)
