544c396b475de88c2ec2d564b303fdd24462e272
SkyRanger05 <120250815+SkyRanger05@users.noreply.github.com>
2022-12-30 22:20:08 -0800

    More dictionary

---
+++ UC_BERKLEY_BOOTCAMP_FinTech_coding-exercises/hello_dicts-01_world.py
@@ -0,0 +1,22 @@
+# san_francisco = {
+#     "has_multiple_bridges": True,
+#     "state": "California",
+#     "tallest_building": "SalesForce Building",
+#     "median_house_price": 1610000,
+#     "notable_attractions": ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"],
+# }
+
+# Re-create the content of the commented out `san_francisco_rev1` dictionary by using bracket notation to manually add each of the key-value pairs (including nested objects).
+san_francisco_rev1 = {}
+notable_attractions = ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"]
+san_francisco_rev1["has_multiple_bridges"] = "True"
+san_francisco_rev1["state"] = "California"
+san_francisco_rev1["tallest_building"] = "SalesForce Building"
+san_francisco_rev1["median_house_price"] = 1610000
+san_francisco_rev1["notable_attractions"] = notable_attractions
+
+
+# Print the manually modified `san_francisco` dictionary and confirm the contents match the commented out version.
+print()
+print(san_francisco_rev1)
+print()
