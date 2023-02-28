6e178b1ed4ddf2e28d76949cb6a2b89162d44a44
skyranger05 <ian.capri@live.com>
2023-02-28 11:14:14 -0800

    Addes List exercise

---
+++ UC_BERKLEY_BOOTCAMP_FinTech_coding-exercises/Module1/list.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+color_pants = ["blue", "black", "green", "white", "red"]
+
+cool_things = ['Lamborgine', 42, ["burrito", "nachos", "tacos"], "Brazil", ]
+
+print(color_pants, cool_things)
+
+where_we_live = [
+    "San Bay Area"
+    "Los Aligas"
+    "Beverbe Drive"
+    "Evergreen Hills"
+]
+
+where_we_live.append("Ward")
+print(where_we_live)
