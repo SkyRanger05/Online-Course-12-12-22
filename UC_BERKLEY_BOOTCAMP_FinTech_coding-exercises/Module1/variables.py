c51f5f5f4e34f759064c781f0e80f664b15c68ed
skyranger05 <ian.capri@live.com>
2023-02-28 23:55:48 -0800

    Added new exercise

---
+++ UC_BERKLEY_BOOTCAMP_FinTech_coding-exercises/Module1/variables.py
@@ -0,0 +1,72 @@
+# Create variables
+floor_area_sq_ft = 1500
+sale_item = "dining table"
+auto_maintenance = "oil change"
+curriculum = "English, Math, Science"
+# Print the variables
+print(floor_area_sq_ft, sale_item, auto_maintenance, curriculum)
+# Prints the data type of each declared variable
+print(type(floor_area_sq_ft))
+print(type(sale_item)) 
+print (type(auto_maintenance))
+print(type(curriculum))
+# Using variable names in calculations
+date_of_birth_year = 1976
+current_year = 2022
+age = current_year - date_of_birth_year
+print(age)
+# Updating variables using assignment
+print()
+subject = None
+print(subject)
+print()
+
+print()
+subject = "Ada"
+print(subject)
+print()
+
+print()
+subject = ""
+print(subject)
+print()
+
+print()
+subject = "James"
+print(subject)
+print()
+
+"""
+Rate = distance/time
+"""
+distance = 60
+time = 60
+rate = distance / time
+print(rate)
+
+# Substituting/formatting variable
+print(f"The house has about {floor_area_sq_ft} sq. ft' of living space.")
+print(f"This {sale_item} is for sale at $70.")
+print(f"On Saturday, I will do {auto_maintenance} my Ferrari.")
+print(f"This fall, I will be signing up classes for {curriculum}")
+print(f"I'm {age} years of age.")
+message = f"He is going {rate} mph."
+print(message)
+# Two number values will be added
+print()
+groceries_item1 = 4.99
+groceries_item2 = 7.34
+total_cost = groceries_item1 + groceries_item2
+groceries = f"The total groceries is {total_cost}"
+print(groceries)
+print()
+# Two string values will be concatenated
+where_is_samantha = "Where is Samantha! Her mother replied, 'just over the hill pass the oak tree, a breeze five mile walk, might have to cross the brook as wide as highways since it rained heavily two days ago, then turn east continue tophill to Mrs. Bloomdale victorian house, behind the house there is a lookout bench, and you might find her there!.'"
+why_samantha_went = " Samatha likes to see the sunset as it takes a dip into the dark night autumn sky!"
+samantha = where_is_samantha + why_samantha_went
+print(samantha)
+# Variable naming conventions
+kpg = 43
+
+# Better
+king_pin_golfer = 43
