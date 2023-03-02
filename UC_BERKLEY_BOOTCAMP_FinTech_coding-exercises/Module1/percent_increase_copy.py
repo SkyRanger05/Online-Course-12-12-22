507cf9929132bafe9f097bce0dd28f7a5746be43
skyranger05 <ian.capri@live.com>
2023-03-01 23:28:26 -0800

    Added new exercise file

---
+++ UC_BERKLEY_BOOTCAMP_FinTech_coding-exercises/Module1/percent_increase_copy.py
@@ -0,0 +1,26 @@
+# Percent Increase Activity
+
+# Formulas
+# Increase = Current Price - Original Price
+# Percent Increase = Increase / Original x 100
+
+# @TODO: Create float variable for original_price
+original_price = 6.89
+
+# @TODO: Create float variable for current_price
+current_price =18.65
+
+# @TODO: Calculate difference between current_price and original_price
+Increase = current_price - original_price
+
+# @TODO: Calculate percent_increase
+# YOUR CODE HERE!
+
+# Print original_price
+print(f"Apple's original stock price was ${original_price}")
+
+# Print current_price
+print(f"Apples current stock price is ${current_price}")
+
+# @TODO: Print percent_increase to 2 decimal places using f-string formatting
+# YOUR CODE HERE!
